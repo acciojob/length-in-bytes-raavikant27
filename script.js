0abf12be2bf1164d849ad25f09fb20e80b860781
Ravikant Singh <122196249+raavikant27@users.noreply.github.com>
2025-03-30 13:13:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+
 const byteSize = (str) => {
-  // write your code here
+  // Create a new Blob with the given string and get its size in bytes
+  return new Blob([str]).size;
 };
+
+// Do not change the code below
+const str = prompt("Enter some string.");
+alert(byteSize(str));
 
 // Do not change the code below
 const str = prompt("Enter some string.");
